f8792266f869801af13aef69a49d2d66f0842f43
Givikap120 <givikap120@gmail.com>
2024-10-18 23:28:16 +0300

    added pp calc

--- app/rulesets/osu/performance/reading/pp.go
+++ app/rulesets/osu/performance/reading/pp.go
@@ -92,19 +92,66 @@ func (pp *PPv2) Calculate(attribs api.Attributes, combo, n300, n100, n50, nmiss 
 		pp.effectiveMissCount = min(pp.effectiveMissCount+float64(pp.countOk)*okMultiplier+float64(pp.countMeh)*mehMultiplier, float64(pp.totalHits))
 	}
 
-	results := api.PPv2Results{
-		Aim:        pp.computeAimValue(),
-		Speed:      pp.computeSpeedValue(),
-		Acc:        pp.computeAccuracyValue(),
-		Flashlight: pp.computeFlashlightValue(),
+	result := pp.calculatePPv2Results()
+
+	pp.effectiveMissCount = 0
+	pp.countMiss = 0
+	pp.scoreMaxCombo = pp.attribs.MaxCombo
+	multiplier *= pp.calculateBalanceAdjustingMultiplier()
+
+	result.Total *= multiplier
+	return result
+}
+
+func (pp *PPv2) calculatePPv2Results() api.PPv2Results {
+	aimValue := pp.computeAimValue()
+	speedValue := pp.computeAimValue()
+
+	lowARValue := pp.computeAimValue()
+	highARValue := pp.computeAimValue()
+
+	potentialFlashlightValue := pp.computeAimValue()
+	hiddenValue := pp.computeAimValue()
+
+	flashlightValue := 0.0
+	if pp.diff.Mods.Active(difficulty.Flashlight) {
+		flashlightValue = potentialFlashlightValue
 	}
 
-	results.Total = math.Pow(
-		math.Pow(results.Aim, 1.1)+
-			math.Pow(results.Speed, 1.1)+
-			math.Pow(results.Acc, 1.1)+
-			math.Pow(results.Flashlight, 1.1),
-		1.0/1.1) * multiplier
+	ARValue := math.Pow(
+		math.Pow(lowARValue, 1.1)+
+			math.Pow(highARValue, 1.1),
+		1.0/1.1,
+	)
+
+	flashlightARValue := math.Pow(
+		math.Pow(ARValue, 1.5)+
+			math.Pow(flashlightValue, 1.5),
+		1.0/1.5,
+	)
+
+	cognitionValue := flashlightARValue + hiddenValue
+	mechanicalValue := math.Pow(
+		math.Pow(aimValue, 1.1)+
+			math.Pow(speedValue, 1.1),
+		1.0/1.1,
+	)
+
+	cognitionValue = AdjustCognitionPerformance(cognitionValue, mechanicalValue, potentialFlashlightValue)
+	accValue := pp.computeAccuracyValue()
+	totalValue := cognitionValue + math.Pow(math.Pow(mechanicalValue, 1.1)+math.Pow(accValue, 1.1), 1.0/1.1)
+
+	visualReadingValue := AdjustCognitionPerformance(ARValue+hiddenValue, mechanicalValue, flashlightValue)
+	visualFlashlightValue := cognitionValue - visualReadingValue
+
+	results := api.PPv2Results{
+		Aim:        aimValue,
+		Speed:      speedValue,
+		Acc:        accValue,
+		Flashlight: visualFlashlightValue,
+		Reading:    visualReadingValue,
+		Total:      totalValue,
+	}
 
 	return results
 }
@@ -246,10 +293,6 @@ func (pp *PPv2) computeAccuracyValue() float64 {
 }
 
 func (pp *PPv2) computeFlashlightValue() float64 {
-	if !pp.diff.CheckModActive(difficulty.Flashlight) {
-		return 0
-	}
-
 	flashlightValue := skills.FlashlightDifficultyToPerformance(pp.attribs.Flashlight)
 
 	// Penalize misses by assessing # of misses relative to the total # of objects. Default a 3% reduction for any # of misses.
@@ -274,6 +317,91 @@ func (pp *PPv2) computeFlashlightValue() float64 {
 	flashlightValue *= 0.98 + math.Pow(pp.diff.ODReal, 2)/2500
 
 	return flashlightValue
+}
+
+func (pp *PPv2) computeLowARValue() float64 {
+	readingValue := skills.LowARDifficultyToPerformance(pp.attribs.ReadingDifficultyLowAR)
+
+	// Penalize misses by assessing # of misses relative to the total # of objects. Default a 3% reduction for any # of misses.
+	if pp.effectiveMissCount > 0 {
+		readingValue *= pp.calculateMissPenalty(pp.effectiveMissCount, pp.attribs.LowArDifficultStrainCount)
+	}
+
+	readingValue *= pp.accuracy * pp.accuracy
+	readingValue *= math.Pow(0.98+math.Pow(pp.diff.ODReal, 2)/2500, 2)
+
+	return readingValue
+}
+
+func (pp *PPv2) computeHighARValue() float64 {
+	highARValue := skills.DefaultDifficultyToPerformance(pp.attribs.ReadingDifficultyHighAR)
+
+	// Approximate how much of high AR difficulty is aim
+	aimPerformance := skills.DefaultDifficultyToPerformance(pp.attribs.Aim)
+	speedPerformance := skills.DefaultDifficultyToPerformance(pp.attribs.Speed)
+
+	aimRatio := aimPerformance / (aimPerformance + speedPerformance)
+
+	// Aim part calculation
+	aimPartValue := highARValue * aimRatio
+	// We assume 15% of sliders in a map are difficult since there's no way to tell from the performance calculator.
+	estimateDifficultSliders := float64(pp.attribs.Sliders) * 0.15
+
+	if pp.attribs.Sliders > 0 {
+		estimateSliderEndsDropped := mutils.Clamp(float64(min(pp.countOk+pp.countMeh+pp.countMiss, pp.attribs.MaxCombo-pp.scoreMaxCombo)), 0, estimateDifficultSliders)
+		sliderNerfFactor := (1-pp.attribs.SliderFactor)*math.Pow(1-estimateSliderEndsDropped/estimateDifficultSliders, 3) + pp.attribs.SliderFactor
+		aimPartValue *= sliderNerfFactor
+	}
+
+	aimPartValue *= pp.accuracy
+	// It is important to also consider accuracy difficulty when doing that
+	aimPartValue *= 0.98 + math.Pow(pp.diff.ODReal, 2)/2500
+
+	// Speed part calculation
+	speedPartValue := highARValue * (1 - aimRatio)
+	relevantAccuracy := 0.0
+	if pp.attribs.SpeedNoteCount != 0 {
+		relevantTotalDiff := float64(pp.totalHits) - pp.attribs.SpeedNoteCount
+		relevantCountGreat := max(0, float64(pp.countGreat)-relevantTotalDiff)
+		relevantCountOk := max(0, float64(pp.countOk)-max(0, relevantTotalDiff-float64(pp.countGreat)))
+		relevantCountMeh := max(0, float64(pp.countMeh)-max(0, relevantTotalDiff-float64(pp.countGreat)-float64(pp.countOk)))
+		relevantAccuracy = (relevantCountGreat*6.0 + relevantCountOk*2.0 + relevantCountMeh) / (pp.attribs.SpeedNoteCount * 6.0)
+	}
+
+	// Scale the speed value with accuracy and OD
+	speedPartValue *= (0.95 + math.Pow(pp.diff.ODReal, 2)/750) * math.Pow((pp.accuracy+relevantAccuracy)/2.0, (14.5-pp.diff.ODReal)/2)
+
+	// Scale the speed value with # of 50s to punish doubletapping.
+	if float64(pp.countMeh) >= float64(pp.totalHits)/500 {
+		speedPartValue *= math.Pow(0.99, float64(pp.countMeh)-float64(pp.totalHits)/500.0)
+	}
+
+	return aimPartValue + speedPartValue
+}
+
+func (pp *PPv2) computeHiddenValue() float64 {
+	if !pp.diff.CheckModActive(difficulty.Hidden) {
+		return 0
+	}
+
+	readingValue := skills.HiddenDifficultyToPerformance(pp.attribs.HiddenDifficulty)
+
+	lengthBonus := 0.95 + 0.4*min(1.0, float64(pp.totalHits)/2000.0)
+	if pp.totalHits > 2000 {
+		lengthBonus += math.Log10(float64(pp.totalHits)/2000.0) * 0.5
+	}
+
+	readingValue *= lengthBonus
+
+	// Penalize misses by assessing # of misses relative to the total # of objects. Default a 3% reduction for any # of misses.
+	if pp.effectiveMissCount > 0 {
+		readingValue *= pp.calculateMissPenalty(pp.effectiveMissCount, pp.attribs.HiddenDifficultStrainCount)
+	}
+
+	readingValue *= pp.accuracy * pp.accuracy
+	readingValue *= 0.98 + math.Pow(pp.diff.ODReal, 2)/2500
+
+	return readingValue
 }
 
 func (pp *PPv2) calculateEffectiveMissCount() float64 {
@@ -318,7 +446,19 @@ func AdjustCognitionPerformance(cognitionPerformance, mechanicalPerformance, fla
 	return ratio * capPerformance
 }
 
+func (pp *PPv2) calculateBalanceAdjustingMultiplier() float64 {
+	totalValue := pp.calculatePPv2Results().Total * PerformanceBaseMultiplier
+
+	if totalValue < 600 {
+		return 1
+	}
+
+	rescaledValue := (totalValue - 600) / 1000
+	result := min(0.06*rescaledValue, 0.088*math.Pow(rescaledValue, 0.4))
+	return 1 + result
+}
+
 // softmin is a function that computes a soft minimum between two values with an optional power argument
-func softmin(a, b float64) float64 {
-	return a * b / math.Log(math.Exp(a)+math.Exp(b))
+func softmin(a, b, power float64) float64 {
+	return a * b / math.Log(math.Pow(power, a)+math.Pow(power, b))
 }
